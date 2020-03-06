d206665a35e87493026c522bed92515c52424eac
1drees <56352832+1drees@users.noreply.github.com>
2020-03-06 13:05:44 +0100

    Finished Members.js

--- docs/Members.js
+++ docs/Members.js
@@ -1,79 +1,61 @@
-<<<<<<< HEAD
-
-/* 
-//Get and set values for Member list
-=======
 //IDs
-const inputField = "member-input";
-const divOutputField = "member-output";
-=======
-// <<<<<<< HEAD
-// //Get and set values for Member list
-// =======
-// //IDs
-// const inputField = "member-input";
-// const divOutputField = "member-output";
->>>>>>> fb9a495e248074618a69d6f65c34245bf2e2cc2b
+const inputField1 = "member-input";
+const divOutputField1 = "member-output";
 
-// //Get and set values for Member list into localStorage
-// function createMember(){
-//     //prevents the website from automaticly updating
-//     //(prevents default action)
-//     event.preventDefault();
-//     //get inputfield value
-//     const member = document.getElementById(inputField).value;
-//      //Grab array from localstorage
-//      const memberList = JSON.parse(localStorage.getItem('memberList'));
-//      //Add new input to array
-//     memberList.push(member);
+//Get and set values for Member list into localStorage
+function createMember(){
+    //prevents the website from automaticly updating
+    //(prevents default action)
+    event.preventDefault();
+    //get inputfield value
+    const member = document.getElementById(inputField1).value;
+     //Grab array from localstorage
+     const memberList = JSON.parse(localStorage.getItem('memberList')) ?? [];
+     //Add new input to array
+    memberList.push(member);
 
-//     //place the array back in local storage
-//     localStorage.setItem('memberList', JSON.stringify(memberList));
+    //place the array back in local storage
+    localStorage.setItem('memberList', JSON.stringify(memberList));
 
-//     //output
-//     displayMembers();
-//     //clears input field
-//     event.target.reset();
-// }
+    //output
+    displayMembers();
+    //clears input field
+    event.target.reset();
+    //refers to the tasklist
+    displayTasks();
+}
 
-// //Get tasklist and memberlist from localStorage
-// //Display tasklist as LI elements and members as option elements
-// function displayMembers(){
-//     //Get output position
-//     const divOutput = document.getElementById(divOutputField);
-//     //Get localStorage lists
-//     const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
-//     const memberList = JSON.parse(localStorage.getItem('memberList')) || [];
-//     //Clear list
-//     divOutput.innerHTML = "";
-//     //Create select container
-//     const selectEl = document.createElement("select");
-//     //Add members into select element
-//     for(const member of memberList) {
-//         selectEl.innerText += `<option>${member}</option>`;
-//     }
-//     //add tasks into output div as li elements
-//     for(const task of taskList) {
-//         //Create list element
-//         const liElement = document.createElement("LI");
-//         //check if the task field is empty
-//         //if not add tasks and members to li element
-//         if(task != "") {
-//         liElement.innerText = `<h1>${task}</h1> <select>${selectEl.innerText}</select>`;
-//         }
-//         //output to the screen
-//         divOutput.innerHTML += liElement.innerText;
-//     }
+//Get tasklist and memberlist from localStorage
+//Display tasklist as LI elements and members as option elements
+function displayMembers(){
+
+    //Get output position
+    const divOutput = document.getElementById(divOutputField1);
+
+    //Get localStorage lists
+    const memberList = JSON.parse(localStorage.getItem('memberList')) || [];
+
+    //Clear list
+    divOutput.innerHTML = "";
+
+    //add tasks into output div as li elements
+    for(const member of memberList) {
+
+        //Create list element
+        const liElement = document.createElement("LI");
+
+        //check if the task field is empty
+        //if not add tasks and members to li element
+        if(member != "") {
+        liElement.innerText = `<h1>${member}</h1>`;
+        }
+
+        //output to the screen
+        divOutput.innerHTML += liElement.innerText;
+    }
 
     
-// }
+}
 
-<<<<<<< HEAD
-//Display tasks on page load
+//Display members on page load
 displayMembers();
->>>>>>> c376a06841cdc53b97c2fd2ec986277b647605f7 */
-=======
-// //Display tasks on page load
-// displayMembers();
-// >>>>>>> c376a06841cdc53b97c2fd2ec986277b647605f7
->>>>>>> fb9a495e248074618a69d6f65c34245bf2e2cc2b
